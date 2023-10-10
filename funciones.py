fef1a0cb926b22f34432dae72c8dd73b3600ef09
Nicolas Andrade <nandrade_red@COORDINACION.LAN>
2023-10-10 11:42:46 -0300

    agregamos menu de opciones

---
+++ funciones.py
@@ -0,0 +1,13 @@
+def menu():
+    print("1.Crear archivo binario de registros")
+    print("2.cargar por teclado los datos de un tiket")
+    print("3.Mostrar todos los datos de todos los registros del archivo binario.")
+    print("4.Mostrar todos los registros del archivo binario.")
+    print("5.Buscarsi existe en el archivo binario un tiket cargado por teclado")
+    print(
+        "6.Determinar y mostrar la cantidad de vehiculos de cada combinacion posible entre tipo de vehiculo y pais de cabina en el archivo binario")
+    print(
+        "7. muestre la cantidad total de vehículos contados por cada tipo de vehículo posible, y la cantidad total de vehículos contados por cada país de cabina posible.")
+    print(
+        "8. Calcular y mostrar la distancia promedio desde la última cabina recorrida entre todos los vehículos del archivo binario")
+
---
+++ main.py
@@ -0,0 +1,9 @@
+from funciones import *
+
+
+def principal():
+    menu()
+
+
+if __name__ == '__main__':
+    principal()
